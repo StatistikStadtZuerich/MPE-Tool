f86f95df7c610dd7ddb826a6fd04ba230ace72e0
Hänzi Sara (SSZ) <Sara.Haenzi@zuerich.ch>
2022-10-31 17:32:11 +0100

    modify for test and production app

--- deploy.R
+++ deploy.R
@@ -3,4 +3,5 @@ rsconnect::setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
                token = Sys.getenv("TOKEN"),
                secret = Sys.getenv("SECRET"))
 # Deploy
-rsconnect::deployApp(forceUpdate = TRUE)
+rsconnect::deployApp(forceUpdate = TRUE,
+                     appName = Sys.getenv("APP_NAME"))
