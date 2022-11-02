295429905bf7d5de6b0e2c7a285001f2926f741d
Hänzi Sara (SSZ) <Sara.Haenzi@zuerich.ch>
2022-11-02 16:08:09 +0100

    do not set anything remotely for the app

--- deploy.R
+++ deploy.R
@@ -5,7 +5,3 @@ rsconnect::setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
 # Deploy
 rsconnect::deployApp(forceUpdate = TRUE,
                      appName = Sys.getenv("APP_NAME"))
-
-# adjust memory size to extra large
-rsconnect::configureApp(Sys.getenv("APP_NAME"),
-                        size = "3xlarge")
