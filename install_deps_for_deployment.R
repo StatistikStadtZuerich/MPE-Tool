405778533077c934733eeac8b6dc8966aece4499
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2023-08-21 15:46:39 +0200

    try with installation in a script

---
+++ install_deps_for_deployment.R
@@ -0,0 +1,5 @@
+install.packages("rsconnect", "shiny", "reactable", "lubridate", "jpeg", 
+                 "httr", "data.table", "Rcpp", "openxlsx", "readxl", "remotes")
+remotes::install_github('StatistikStadtZuerich/zuericssstyle')
+remotes::install_github('StatistikStadtZuerich/zuericolors')
+remotes::install_github('mitchelloharawild/icons')
