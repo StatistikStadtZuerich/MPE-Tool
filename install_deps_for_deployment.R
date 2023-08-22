23ce8a8c657193273e88f80863b3c165338fa601
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2023-08-22 10:08:15 +0200

    also install those dependencies in deployment installation script

--- install_deps_for_deployment.R
+++ install_deps_for_deployment.R
@@ -1,5 +1,6 @@
-install.packages(c("rsconnect", "shiny", "reactable", "lubridate", "jpeg", 
-                   "httr", "data.table", "Rcpp", "openxlsx", "readxl", "pak"))
+install.packages(c("data.table", "dplyr", "forcats", "httr", "jpeg", "lubridate", 
+                   "openxlsx", "pak", "Rcpp", "reactable", "readxl", 
+                   "rsconnect", "shiny", "tidyr"))
 pak::pkg_install('StatistikStadtZuerich/zuericssstyle')
 pak::pkg_install('StatistikStadtZuerich/zuericolors')
 pak::pkg_install('mitchelloharawild/icons')
