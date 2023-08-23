87f9607db94031b067fabb3ff982e85f68502660
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2023-08-23 11:27:26 +0200

    try old way of dependency detection, worked with abda

--- deploy.R
+++ deploy.R
@@ -2,6 +2,10 @@
 rsconnect::setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
                token = Sys.getenv("TOKEN"),
                secret = Sys.getenv("SECRET"))
+
+# use the old way of finding dependencies; can be removed with update or R version in Dockerfile and development
+options(rsconnect.packrat = TRUE)
+
 # Deploy
 rsconnect::deployApp(forceUpdate = TRUE,
                      appName = Sys.getenv("APP_NAME"),
