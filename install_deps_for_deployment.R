290cc14619e4a46e926fb4faca5989e26ceabed2
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2023-08-30 09:47:13 +0200

    delete files that are not necessary for gitlab deployment

--- install_deps_for_deployment.R
+++
@@ -1,6 +0,0 @@
-install.packages(c("data.table", "dplyr", "forcats", "httr", "jpeg", "lubridate", 
-                   "openxlsx", "pak", "Rcpp", "reactable", "readxl", 
-                   "rsconnect", "shiny", "tidyr"))
-pak::pkg_install('StatistikStadtZuerich/zuericssstyle')
-pak::pkg_install('StatistikStadtZuerich/zuericolors')
-pak::pkg_install('mitchelloharawild/icons')
